ba61cff8a2d119085def0f450da1edafa0ae715b
Nitin Surya Imadabathuni <65825453+initinsurya@users.noreply.github.com>
2023-02-15 19:35:46 -0500

    baseURL change

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
   //baseURL: "http://localhost:8000/jat",
-  baseURL: "https://jat-appl-tracker.herokuapp.com/jat",
+  //baseURL: "https://jat-appl-tracker.herokuapp.com/jat",
+  baseURL: "https://possible-phase-spoon.glitch.me/jat/getAllJobs",
 });
 
 export default instance;
