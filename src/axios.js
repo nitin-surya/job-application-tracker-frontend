924b67447b1f298ee847cce4e0da69fad5036ed4
Nitin Surya Imadabathuni <65825453+initinsurya@users.noreply.github.com>
2023-02-17 00:37:04 -0500

    Cyclic -BaseUrl

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //baseURL: "http://localhost:8000/jat",
-  //baseURL: "https://jat-appl-tracker.herokuapp.com/jat",
-  baseURL: "https://possible-phase-spoon.glitch.me/jat",
+  /* baseURL: "http://localhost:8000/jat",/
+  /* Heroku - baseURL: "https://jat-appl-tracker.herokuapp.com/jat",/
+  /* Glitch - baseURL: "https://possible-phase-spoon.glitch.me/jat",/  
+  /* Cyclic */ baseURL: "https://handsome-teal-walkingstick.cyclic.app/jat",
 });
 
 export default instance;
