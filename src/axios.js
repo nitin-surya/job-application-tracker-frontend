23aac9cadaa81a1ba36537f5a70f852bc693b05a
Nitin Surya <nitinsuryaimadabathuni@gmail.com>
2024-08-25 13:08:01 -0500

    baseURL change

--- src/axios.js
+++ src/axios.js
@@ -4,11 +4,9 @@ const instance = axios.create({
   /* baseURL: "http://localhost:8000/jat",/
   /* Heroku - baseURL: "https://jat-appl-tracker.herokuapp.com/jat",/
   /* Glitch - baseURL: "https://possible-phase-spoon.glitch.me/jat",/  
-  /* Cyclic */ baseURL: "https://handsome-teal-walkingstick.cyclic.app/jat",
+  /* Cyclic  baseURL: "https://handsome-teal-walkingstick.cyclic.app/jat", */
+  /* Render*/ baseURL:
+    "https://job-application-tracker-backend-s1bt.onrender.com/jat",
 });
 
 export default instance;
-
-
-
-
