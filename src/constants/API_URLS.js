aa91ceef33bc7eb9a0b647d5ef28b68108f4a52e
Nitin Surya <nitinsuryaimadabathuni@gmail.com>
2024-08-29 01:21:56 -0500

    application url change

--- src/constants/API_URLS.js
+++ src/constants/API_URLS.js
@@ -3,6 +3,6 @@ export const ADD_JOB = "/jobs/addJob";
 export const EDIT_JOB = "/jobs/updateJob";
 export const DELETE_JOB = "/jobs/deleteJob";
 // Define API URLs for authentication
-export const LOGIN_URL = "/job-application-tracker-frontend/login";
-export const REGISTER_URL = "/job-application-tracker-frontend/signup";
+export const LOGIN_URL = "/login";
+export const REGISTER_URL = "/signup";
 export const LOGOUT_URL = "/logout";
